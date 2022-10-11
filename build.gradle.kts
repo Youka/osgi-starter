8f71b71959d195dd7a6df212bbc59a101a1706e1
Youka <spanknebel.borken@t-online.de>
2022-10-11 03:07:47 +0200

    simplify bundling

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,8 +2,4 @@ group = "io.youka"
 version = "1.0-SNAPSHOT"
 
 subprojects {
-    repositories {
-        mavenLocal()
-        mavenCentral()
-    }
 }
--- io.youka.osgi.web_server/src/main/java/io/youka/osgi/web_server/Hello.java
+++ io.youka.osgi.web_server/src/main/java/io/youka/osgi/web_server/Hello.java
@@ -3,10 +3,10 @@ package io.youka.osgi.web_server;
 import org.apache.felix.service.command.annotations.GogoCommand;
 import org.osgi.service.component.annotations.Component;
 
-@GogoCommand(scope="hello", function= {"hello"})
+@GogoCommand(scope="hello", function="world")
 @Component(service=Object.class)
-public class HelloWorld {
-    public String hello() {
+public class Hello {
+    public String world() {
         return "Hello World";
     }
 }
