59dec57be729c1945d088ecfada474394987c00b
Youka <spanknebel.borken@t-online.de>
2022-10-14 16:35:26 +0200

    added resource loading and common library example

--- io.youka.osgi.web_server/src/main/java/io/youka/osgi/web_server/Hello.java
+++ io.youka.osgi.web_server/src/main/java/io/youka/osgi/web_server/Hello.java
@@ -1,14 +1,22 @@
 package io.youka.osgi.web_server;
 
 import lombok.Getter;
+import org.apache.commons.io.IOUtils;
 import org.apache.felix.service.command.annotations.GogoCommand;
 import org.osgi.service.component.annotations.Component;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
 @GogoCommand(scope="hello", function="world")
 @Component(service=Object.class)
 public class Hello {
     @Getter
-    private final String world = "Hello World";
+    private final String world;
+
+    public Hello() throws IOException {
+        this.world = IOUtils.resourceToString("/hello_world.txt", StandardCharsets.UTF_8, this.getClass().getClassLoader());
+    }
 
     public String world() {
         return this.getWorld();
