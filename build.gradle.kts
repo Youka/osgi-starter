4857b57f7cb71b82f97bd1b673fbc5dc341a44b0
Youka <spanknebel.borken@t-online.de>
2022-10-14 16:00:56 +0200

    added project lombok annotation processing

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,25 @@
+// Project meta information
 group = "io.youka"
-version = "1.0-SNAPSHOT"
+version = "1.0.0-SNAPSHOT"
 
+
+// Import api to create resources for subprojects
+plugins {
+    java
+}
+
+// Extend build process for osgi bundles
 subprojects {
+    // Import abi for resources provided by the parent project
+    apply(plugin = "java")
+
+    // Add maven repository for gradle (bndtools uses MavenBndRepository plugin)
+    repositories {
+        mavenCentral()
+    }
+
+    // Extend java compilation by annotation processors
+    dependencies {
+        compileOnly("org.projectlombok:lombok:1.18.24")?.apply { annotationProcessor(this) }
+    }
 }
--- io.youka.osgi.web_server/src/main/java/io/youka/osgi/web_server/Hello.java
+++ io.youka.osgi.web_server/src/main/java/io/youka/osgi/web_server/Hello.java
@@ -1,12 +1,16 @@
 package io.youka.osgi.web_server;
 
+import lombok.Getter;
 import org.apache.felix.service.command.annotations.GogoCommand;
 import org.osgi.service.component.annotations.Component;
 
 @GogoCommand(scope="hello", function="world")
 @Component(service=Object.class)
 public class Hello {
+    @Getter
+    private final String world = "Hello World";
+
     public String world() {
-        return "Hello World";
+        return this.getWorld();
     }
 }
