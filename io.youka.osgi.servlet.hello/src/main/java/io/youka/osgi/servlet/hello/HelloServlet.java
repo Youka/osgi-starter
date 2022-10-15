3cc8e32ae67d647d1fe316b9485036f44824afc3
Youka <spanknebel.borken@t-online.de>
2022-10-15 14:02:03 +0200

    simplified servlet registration

--- io.youka.osgi.servlet.hello/src/main/java/io/youka/osgi/servlet/hello/HelloServlet.java
+++ io.youka.osgi.servlet.hello/src/main/java/io/youka/osgi/servlet/hello/HelloServlet.java
@@ -1,13 +1,19 @@
 package io.youka.osgi.servlet.hello;
 
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.http.whiteboard.propertytypes.HttpWhiteboardServletPattern;
+
+import javax.servlet.Servlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+@HttpWhiteboardServletPattern("/hello")
+@Component(service = Servlet.class)
 public class HelloServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-        resp.getWriter().print("Hello world!");
+        resp.getWriter().print("Hello world");
     }
 }
