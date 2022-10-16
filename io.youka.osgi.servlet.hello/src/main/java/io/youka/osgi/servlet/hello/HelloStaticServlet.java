31e00fd79e97d4922dd48db08fca4e6e71816a10
Youka <spanknebel.borken@t-online.de>
2022-10-16 11:03:41 +0200

    fix hello servlet paths

--- io.youka.osgi.servlet.hello/src/main/java/io/youka/osgi/servlet/hello/HelloStaticServlet.java
+++ io.youka.osgi.servlet.hello/src/main/java/io/youka/osgi/servlet/hello/HelloStaticServlet.java
@@ -3,7 +3,7 @@ package io.youka.osgi.servlet.hello;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.http.whiteboard.propertytypes.HttpWhiteboardResource;
 
-@HttpWhiteboardResource(pattern = "/hello/*", prefix = "/")
+@HttpWhiteboardResource(pattern = "/hello-static/*", prefix = "/www")
 @Component(service = HelloStaticServlet.class)
 public class HelloStaticServlet {
 }
