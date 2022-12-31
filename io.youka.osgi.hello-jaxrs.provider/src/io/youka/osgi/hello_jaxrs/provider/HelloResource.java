e55626a0fd5f35ae70ae125941c5dd4ff7e29b9b
Youka <spanknebel.borken@t-online.de>
2022-12-31 16:30:19 +0100

    fix run bundles for jax-rs (by resolve mechanic)

--- io.youka.osgi.hello-jaxrs.provider/src/io/youka/osgi/hello_jaxrs/provider/HelloResource.java
+++ io.youka.osgi.hello-jaxrs.provider/src/io/youka/osgi/hello_jaxrs/provider/HelloResource.java
@@ -3,6 +3,7 @@ package io.youka.osgi.hello_jaxrs.provider;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.jaxrs.whiteboard.propertytypes.JaxrsResource;
 
+import javax.annotation.security.PermitAll;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 
@@ -12,6 +13,7 @@ import javax.ws.rs.Path;
 public class HelloResource {
     @GET
     @Path("/")
+    @PermitAll
     public String hello() {
         return "Hello world!";
     }
